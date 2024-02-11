932e10bce92eb4957cc6d54d7aaaf41bd512983f
RominaNavarrete <rominagnavarrete@gmail.com>
2024-02-11 16:26:50 -0300

    introduciendo bucles y condicionales en html

--- plantillas/plantillas/views.py
+++ plantillas/plantillas/views.py
@@ -4,5 +4,6 @@ def simple(request):
     return render(request, 'simple.html',{})
 
 def dinamico(request, name):
-    context = {'name': name}
+    categories = ['code','desing','marketing','bussines']
+    context = {'name': name, 'categories': categories}
     return render(request, 'dinamico.html', context)
